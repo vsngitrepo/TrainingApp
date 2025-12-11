0bb6131787309bcf5cb0d1f4ed22ce285e790f8d
vsngitrepo <nsvallinikhil@gmail.com>
2025-12-11 13:40:17 -0600

    TestClass4.java is added

---
+++ TestProject/src/TestClass3.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+
+/**
+ * @author vsenthilnathan
+ *
+ */
+public class TestClass {
+       
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("From test Class 3");
+
+	}
+
+}
